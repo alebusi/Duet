2217c8c2b2aeebafa657ee367ab8a41d92c8cd69
KIRA009 <shohanduttaroy99@gmail.com>
2019-05-29 01:50:18 +0530

    Added powerups

--- index.js
+++ index.js
@@ -15,11 +15,23 @@ var Game = function() {
 		self.ctx = self.canvas.getContext('2d');
 		
 		self.score_pos = self.scores.length;
+		name = 'l';
 		var name = prompt('Enter name: ');
+		while (name == '' || name == null)
+			var name = prompt('Enter name: ');
 		self.canvas.style.display = 'block';
 		self.scores.push(new Score(name));
 		this.change_score(0);
 
+		if (vw < 600) {
+			score_board.style.left = '0px';
+			toggle_score();
+		}
+		else {
+			show.style.display = 'none';
+			info.style.top = 30 + score_board.offsetHeight + 'px';
+		}
+		info.style.color = 'white';
 		
 		self.ongoing = false;
 		self.game_over = false;
@@ -36,17 +48,39 @@ var Game = function() {
 		self.deg_jump = 1;
 		
 		orbit = new Circle(0.5 * vw, vh - 150, 100, 0, '31bbfc');
-		wBall = new Circle(orbit.cx - orbit.rad, orbit.cy, 20, 180, 'ffffff', 'white ball');
-		bBall = new Circle(orbit.cx + orbit.rad, orbit.cy, 20, 0, '000000', 'black ball');
+		wBall = new Circle(orbit.cx - orbit.rad, orbit.cy, 20, 180, 'ffffff');
+		bBall = new Circle(orbit.cx + orbit.rad, orbit.cy, 20, 0, '000000');
 		hurdles = [];
+		powerups = [];
+		powerup_names = ['horlicks', 'flight'];
+		last_powerup = 0;
 		painting = 0;
 		kd = false;
 	}
 	this.change_score = function(ds) {
 		self.scores[self.score_pos].score += ds;
+		
 		var val = self.scores[self.score_pos].score;
-		add_score();
-		if (val % 8 == 0 && val > 0) {
+		var score_pos = self.score_pos;
+		var val = self.scores[score_pos];
+		for (var i=score_pos - 1; i>=0; i--) {
+			if (self.scores[i].score < val.score) {
+				self.scores[self.score_pos] = self.scores[i]; // [0]
+				self.score_pos -= 1;
+			}
+			else {
+				break;
+			}
+		}
+		self.scores[self.score_pos] = val;
+		
+		var tbody = document.getElementsByTagName('tbody')[0];
+		var trs = self.scores.slice(0, 5);
+		tbody.innerHTML = '<tr><td>Name</td><td>Score</td></tr>';
+		for (var tr=0; tr<trs.length; tr++)
+			tbody.appendChild(make_score(trs[tr]));
+
+		if (val % 5 == 0 && val > 0) {
 			self.speed += 1;
 			clearInterval(painting);
 			clearInterval(hurdle_factory);
@@ -64,29 +98,6 @@ var Game = function() {
 		tr.appendChild(td);
 		
 		return tr;
-	}
-	var add_score = function() {
-		var score_pos = self.score_pos;
-		var val = self.scores[score_pos];
-		for (var i=score_pos - 1; i>=0; i--) {
-			if (self.scores[i].score < val.score) {
-				self.scores[self.score_pos] = self.scores[i]; // [0]
-				self.score_pos -= 1;
-			}
-			else {
-				break;
-			}
-		}
-		self.scores[self.score_pos] = val;
-		
-		draw_score();
-	}
-	var draw_score = function() {
-		var tbody = document.getElementsByTagName('tbody')[0];
-		var trs = self.scores.slice(0, 5);
-		tbody.innerHTML = '<tr><td>Name</td><td>Score</td></tr>';
-		for (var tr=0; tr<trs.length; tr++)
-			tbody.appendChild(make_score(trs[tr]));
 	}
 	this.move_left = function() {
 		deg -= self.deg_jump;
@@ -149,6 +160,7 @@ var Game = function() {
 			return;
 		this.ongoing = true;
 		self.painter.create_hurdles();
+		self.painter.create_powerups();
 		painting = setInterval(function() {
 			self.painter.draw();
 		}, 20 - self.speed);
@@ -170,7 +182,7 @@ var Game = function() {
 				self.start();
 			}
 			else
-				toggle(0);
+				toggle_state(0);
 		}
 		return;
 	}
@@ -181,7 +193,17 @@ var Game = function() {
 			self.start();
 		}
 		else if (!self.ongoing) {
-			self.start();
+			setTimeout(function() {
+				pause.innerHTML = 'Pause';
+				self.start();
+			}, 3000);
+			pause.innerHTML = 'Restart in 3s'
+			setTimeout(function() {
+				pause.innerHTML = 'Restart in 2s'
+			}, 1000);
+			setTimeout(function() {
+				pause.innerHTML = 'Restart in 1s'
+			}, 2000);
 		}
 	}
 
@@ -198,36 +220,50 @@ var Painter = function(canvas, ctx) {
 	canvas.height = vh;
 	canvas.width = vw;
 	var fill_rect = function(el) {
-		var rect = new Path2D();
+		ctx.beginPath();
+		ctx.rect(el.x, el.y, el.width, el.height);
 		ctx.fillStyle = el.color;
-		rect.rect(el.x, el.y, el.width, el.height);
-		ctx.fill(rect);
+		ctx.closePath();
+		ctx.fill();
 	}
 
 	var stroke_circle = function(el) {
-		var circle = new Path2D();
+		ctx.beginPath();
+		ctx.arc(el.cx, el.cy, el.rad, 0, 2*Math.PI);
 		ctx.strokeStyle = el.color;
-		circle.arc(el.cx, el.cy, el.rad, 0, 2*Math.PI);
-		ctx.stroke(circle);
+		ctx.closePath();
+		ctx.stroke();
 	}
 
 	var fill_circle = function(el) {
-		var circle = new Path2D();
+		ctx.beginPath();
+		ctx.arc(el.cx, el.cy, el.rad, 0, 2*Math.PI);
 		ctx.fillStyle = el.color;
-		circle.arc(el.cx, el.cy, el.rad, 0, 2*Math.PI);
-		ctx.fill(circle);
+		ctx.closePath();
+		ctx.fill();
 	}
 
 	this.draw = function() {
 		ctx.clearRect(0, 0, vw, vh);
+		ctx.beginPath();
+		ctx.rect(0, 0, vw, vh);
 		ctx.fillStyle = '#965784';
-		ctx.fillRect(0, 0, vw, vh);
+		ctx.fill();
+		ctx.closePath();
 		stroke_circle(orbit);
 		fill_circle(wBall);
 		fill_circle(bBall);
 		for (var i=0; i<hurdles.length; i++) {
+			ctx.beginPath();
 			fill_rect(hurdles[i]);
+			ctx.closePath();
 			hurdles[i].move();
+		}
+		for (var i=0; i<powerups.length; i++) {
+			ctx.beginPath();
+			fill_circle(powerups[i]);
+			ctx.closePath();
+			powerups[i].move();
 		}
 	}
 
@@ -237,6 +273,14 @@ var Painter = function(canvas, ctx) {
 			var width = 80 + Math.random() * 40;
 			hurdles.push(new Hurdle(x, width, '1c3a56', vw, vh));
 		}, 1000 - game.speed * 200);
+	}
+	this.create_powerups = function() {
+		powerup_factory = setInterval(function() {
+			// last_powerup += 1;
+			if (Math.random() < 0.05) {
+				powerups.push(new PowerUp(powerup_names[Math.round(Math.random())]));
+			}
+		}, 1000);
 	}
 	this.clear = function() {
 		ctx.clearRect(0, 0, vw, vh);
@@ -249,13 +293,12 @@ var Painter = function(canvas, ctx) {
 /*---------------------------------------------           Circle constructor        ----------------------------------------------- */
 /*--------------------------------------------------------------------------------------------------------------------------------- */
 /*--------------------------------------------------------------------------------------------------------------------------------- */
-var Circle = function(cx, cy, rad, offset, color, name) {
+var Circle = function(cx, cy, rad, offset, color) {
 	var self = this;
 	this.cx = cx;
 	this.cy = cy;
 	this.rad = rad;
 	this.color = '#' + color;
-	this.name = name || 'Noname';
 	this.rotate = function() {
 		self.cx = orbit.cx + orbit.rad * cos(offset + deg);
 		self.cy = orbit.cy + orbit.rad * sin(offset + deg);
@@ -270,6 +313,57 @@ var Circle = function(cx, cy, rad, offset, color, name) {
 var Score = function(name) {
 	this.name = name;
 	this.score = 0;
+}
+
+/*--------------------------------------------------------------------------------------------------------------------------------- */
+/*--------------------------------------------------------------------------------------------------------------------------------- */
+/*--------------------------------------------           Powerup constructor        ----------------------------------------------- */
+/*--------------------------------------------------------------------------------------------------------------------------------- */
+/*--------------------------------------------------------------------------------------------------------------------------------- */
+var PowerUp = function(name) {
+	var self = this;
+	this.cx = orbit.cx + (Math.random() - 0.5) * 2 * orbit.rad;
+	this.cy = 0.1 * vh;
+	this.rad = 20;
+	var balls = [wBall, bBall];
+	this.name = name;
+	if (this.name == 'flight')
+		this.color = 'yellow';
+	else
+		this.color = 'red';
+	var dy = 0.01 * vh;
+
+	this.move = function() {
+		self.cy += dy;
+		for (var i=0; i<2; i++) {
+			if (self.check_y(balls[i])) {
+				if (self.check_x(balls[i])) {
+					if (self.name == 'flight')
+						game.deg_jump = (game.deg_jump * 10 + 1) / 10;
+					else
+						balls[i].rad -= 1;
+
+					self.remove();
+					break;
+				}
+			}
+		}
+		if (self.cy > vh)
+			self.remove();
+	}
+
+	this.check_y = function(ball) {
+		var bar = ball.cy + ball.rad;
+		return (self.cy - self.rad < bar && self.cy + self.rad > bar)
+	}
+	this.check_x = function(ball) {
+		var bar = ball.cx - ball.rad;
+		return ((self.cx - self.rad < bar && self.cx + self.rad > bar) || (self.cx - self.rad > bar && self.cx - self.rad < bar + 2 * ball.rad && self.cx + self.rad > bar))
+	}
+	this.remove = function() {
+		powerups.splice(powerups.indexOf(self), 1);
+	}
+
 }
 
 /*--------------------------------------------------------------------------------------------------------------------------------- */
@@ -291,7 +385,6 @@ var Hurdle = function(x, width, color) {
 		for (var i=0; i<2; i++) {
 			if (self.check_y(balls[i])) {
 				if (self.check_x(balls[i])) {
-					console.log('Collision with ' + balls[i].name);
 					game.game_over = true;
 					game.stop();
 					break;
@@ -338,17 +431,17 @@ function cos(i) {
 /*--------------------------------------------------------------------------------------------------------------------------------- */
 /*--------------------------------------------------------------------------------------------------------------------------------- */
 
-var game = new Game();
-game.init();
-game.draw();
-game.start();
-
 var pause = document.getElementById('pause');
-pause.addEventListener('click', toggle);
+pause.addEventListener('click', toggle_state);
+var show = document.getElementById('show');
+show.addEventListener('click', toggle_score);
+var score_board = document.getElementById('score-board');
+var info = document.getElementById('info');
 
-function toggle(evt) {
+function toggle_state(evt) {
 	if (evt == 0) {
 		pause.innerHTML = 'Restart';
+		info.style.color = 'black';
 		return;
 	}
 	if (game.ongoing) {
@@ -357,6 +450,25 @@ function toggle(evt) {
 	}
 	else {
 		game.resume();
-		pause.innerHTML = 'Pause';
 	}
 }
+
+function toggle_score() {
+	if (score_board.style.left == '-1000px') {
+		show.style.top = 25 + score_board.offsetHeight + 'px';
+		show.innerHTML = 'Hide';
+		score_board.style.left = '20px';
+		info.style.top = 90 + score_board.offsetHeight + 'px';
+	}
+	else {
+		show.style.top = '20px';
+		score_board.style.left = '-1000px';
+		show.innerHTML = 'Show';
+		info.style.top = '85px';
+	}
+}
+
+var game = new Game();
+game.init();
+game.draw();
+game.start();
