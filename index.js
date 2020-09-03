41d2dcbd2c64d4ba1a3d8828e755452d54d30127
alebusi <allebusi@gmail.com>
2020-09-03 12:01:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -184,18 +184,13 @@ class Painter {
 			powerups[i].move();
 		}
 	}
-
-	getRandomInt(min, max) {
-	// Questa funzione genera un numero intero random contenuto fra min e max inclusi
-                return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
 	
 	create_hurdles() {
 		var game = this.game;
 		var hurdles = game.hurdles;
 		this.hurdle_factory = setInterval(function() {
 			//var x = Math.random() * 200;
-			var x = getRandomInt(0,2)/2;
+			var x = (Math.floor(Math.random() * (2 - 0 + 1)) + 0) / 2;
 			var width = 80; // + Math.random() * 40;
 			hurdles.push(new Hurdle(x, width, 'ffffff', window.vw, window.vh));
 		}, 1000 - game.speed * 200);
